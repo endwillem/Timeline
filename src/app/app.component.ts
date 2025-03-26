7b2e7851ef74788d640937400c5e0623e48c935d
Willem van 't End <willem.van.t.end@capgemini.com>
2025-03-26 12:19:02 +0100

    angular routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// elsewhere
+// import {
+//   btnLogin,
+//   btnSignupWithEmailLink,
+//   btnSignup,
+//   btnLogout,
+//   btnUser,
+//   showUser,
+// } from '../ui';
+
+// elsewhere
+// import {
+//   loginEmailPassword,
+//   loginEmailLink,
+//   createAccount,
+//   monitorAuthState,
+//   logout,
+// } from '../auth/auth';
+
+// elsewhere
+// import { getData } from '../database';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ngtimeline';
 }
+
+// elsewhere
+// btnLogin.addEventListener('click', loginEmailPassword);
+// btnSignupWithEmailLink.addEventListener('click', loginEmailLink);
+// btnSignup.addEventListener('click', createAccount);
+// btnLogout.addEventListener('click', logout);
+// btnUser.addEventListener('click', showUser);
+
+// elsewhere?
+// monitorAuthState();
+
+// elsewhere?
+// getData();
--- src/ui.ts
+++ src/ui.ts
@@ -1,52 +1,52 @@
-import { AuthErrorCodes, User } from "firebase/auth";
+import { AuthErrorCodes, User } from 'firebase/auth';
 
-export const login = document.getElementById("login")!;
-export const app = document.getElementById("app")!;
+export const login = document.getElementById('login')!;
+export const app = document.getElementById('app')!;
 
-export const txtEmail = document.getElementById("txtEmail") as HTMLInputElement;
+export const txtEmail = document.getElementById('txtEmail') as HTMLInputElement;
 export const txtPassword = document.getElementById(
-  "txtPassword"
+  'txtPassword'
 ) as HTMLInputElement;
 
-export const btnLogin = document.getElementById("btnLogin")!;
+export const btnLogin = document.getElementById('btnLogin')!;
 export const btnSignupWithEmailLink = document.getElementById(
-  "btnSignupWithEmailLink"
+  'btnSignupWithEmailLink'
 )!;
-export const btnSignup = document.getElementById("btnSignup")!;
-export const btnLogout = document.getElementById("btnLogout")!;
-export const btnUser = document.getElementById("btnUser")!;
+export const btnSignup = document.getElementById('btnSignup')!;
+export const btnLogout = document.getElementById('btnLogout')!;
+export const btnUser = document.getElementById('btnUser')!;
 
-export const divAuthState = document.getElementById("divAuthState")!;
-export const lblAuthState = document.getElementById("lblAuthState")!;
+export const divAuthState = document.getElementById('divAuthState')!;
+export const lblAuthState = document.getElementById('lblAuthState')!;
 
-export const userForm = document.getElementById("userForm")!;
-export const divLoginError = document.getElementById("divLoginError")!;
+export const userForm = document.getElementById('userForm')!;
+export const divLoginError = document.getElementById('divLoginError')!;
 export const lblLoginErrorMessage = document.getElementById(
-  "lblLoginErrorMessage"
+  'lblLoginErrorMessage'
 )!;
 
 export const showLoginForm = () => {
-  login.style.display = "block";
-  app.style.display = "none";
+  login.style.display = 'block';
+  app.style.display = 'none';
 };
 
 export const showUser = () => {
   userForm.style.display =
-    userForm.style.display !== "block" ? "block" : "none";
+    userForm.style.display !== 'block' ? 'block' : 'none';
 };
 
 export const showApp = () => {
-  login.style.display = "none";
-  app.style.display = "block";
+  login.style.display = 'none';
+  app.style.display = 'block';
 };
 
 export const hideLoginError = () => {
-  divLoginError.style.display = "none";
-  lblLoginErrorMessage.innerHTML = "";
+  divLoginError.style.display = 'none';
+  lblLoginErrorMessage.innerHTML = '';
 };
 
 export const showLoginError = (error: any) => {
-  divLoginError.style.display = "block";
+  divLoginError.style.display = 'block';
   if (error.code == AuthErrorCodes.INVALID_PASSWORD) {
     lblLoginErrorMessage.innerHTML = `Wrong password. Try again.`;
   } else {
